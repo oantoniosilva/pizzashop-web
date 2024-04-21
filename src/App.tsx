3749ebacdef2bab2e0cf903c3d1371ebb43a6e52
Antonio <antoniopsilva083@gmail.com>
2024-04-20 23:20:50 -0300

    feat: create login page styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import "./global.css"
 
 import { RouterProvider } from "react-router-dom"
 import { router } from "./routes"
+import { Helmet, HelmetProvider } from "react-helmet-async"
 
 export function App() {
   return (
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
