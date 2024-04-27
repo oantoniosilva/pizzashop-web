bfe1b7a9172e82af2bffba8cfe0996d7eff859d6
Antonio <antoniopsilva083@gmail.com>
2024-04-27 00:15:56 -0300

    feat: create restaurant registration page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { Dashboard } from "./pages/app/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
 import { AuthLayout } from "./pages/_layouts/auth";
+import { SignUp } from "./pages/auth/sign-up";
 
 export const router = createBrowserRouter([
   {
@@ -16,7 +17,8 @@ export const router = createBrowserRouter([
     path: "/",
     element: <AuthLayout />,
     children: [
-      { path: "/sign-in", element: <SignIn /> }
+      { path: "/sign-in", element: <SignIn /> },
+      { path: "/sign-up", element: <SignUp /> }
     ]
   }
 ])
