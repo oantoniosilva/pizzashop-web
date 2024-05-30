8b6865c0291202e5f769e786f17d85be37cd34ca
Antonio Silva <antoniopsilva083@gmail.com>
2024-05-30 12:29:22 -0300

    feat: create header component layout

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,7 +4,9 @@ export function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
-      <h1>Dashboard</h1>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+      </div>
     </>
   )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Dashboard } from "./pages/app/dashboard";
-import { SignIn } from "./pages/auth/sign-in";
+
+import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
+import { Dashboard } from "./pages/app/dashboard/dashboard";
 import { SignUp } from "./pages/auth/sign-up";
+import { SignIn } from "./pages/auth/sign-in";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <AuthLayout />,
+    element: <AppLayout />,
     children: [
       { path: "/", element: <Dashboard /> }
     ]
